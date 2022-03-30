2ba3cc24e3269d7d3848faac8480886ee75b8cb9
filants <antons.filipovs@gmail.com>
2022-03-30 18:39:20 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// REACT 18 - Сначала создание корня через константу
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// REACT 18 - Далее его рендер
+const element = <h1>Hello World!</h1>;
+root.render(element);
